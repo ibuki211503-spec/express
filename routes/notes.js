2a507538db434c11c1fb9729b2acee506fdef525
ibuki211503-spec <ibuki211503@gmail.com>
2025-12-06 23:35:17 +0900

    a

--- routes/notes.js
+++ routes/notes.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://ibuki:ibuki0723@cluster0.uriwxai.mongodb.net/?appName=Cluster0";
+const uri = "HAHAHA";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
