87ec6cd87a0ddbf48db88f9e10ce2ebb6d11668f
ibuki211503-spec <ibuki211503@gmail.com>
2025-12-06 23:02:20 +0900

    add cat api

--- routes/notes.js
+++ routes/notes.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "errerere";
+const uri = "1212";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
